b66a76e0e3822126be50f0938104ef7f042a94b2
⚫️ <jp@bultmann.eu>
2022-12-09 10:35:07 +0100

    make PACT value generic; fixes

--- src/pact.rs
+++ src/pact.rs
@@ -9,79 +9,92 @@ use crate::bitset::{ByteBitset};
 use crate::bytetable::{ByteTable, ByteEntry};
 
 #[repr(C)]
-struct Branch<const N: usize> {
+struct Branch<const N: usize, T: Clone> {
     leaf_count: u64,
     rc: AtomicU16,
     segment_count: u32, //TODO: increase this to a u48
     node_hash: u128,
     child_set: ByteBitset,
-    children: ByteTable<N, Head>,
+    children: ByteTable<N, Head<T>>,
+}
+
+#[repr(C)]
+struct Infix<const N: usize, T: Clone> {
+    child: Head<T>,
+    rc: AtomicU16,
+    infix: [u8; N],
 }
 
 //#[rustc_layout(debug)]
 #[derive(Clone, Debug)]
 #[repr(u8)]
-enum Head {
+enum Head<T: Clone> {
     None {padding: [u8; 15]} = 0,
     Branch1 {
+        infix: [u8; 5],
         start_depth: u8,
         branch_depth: u8,
-        infix: [u8; 5],
-        ptr: NonNull<Branch<1>>,
-        phantom: PhantomData<Branch<1>>
+        ptr: NonNull<Branch<1, T>>,
+        phantom: PhantomData<Branch<1, T>>
         },
     Branch2 {
+        infix: [u8; 5],
         start_depth: u8,
         branch_depth: u8,
-        infix: [u8; 5],
-        ptr: NonNull<Branch<2>>,
-        phantom: PhantomData<Branch<2>>
+        ptr: NonNull<Branch<2, T>>,
+        phantom: PhantomData<Branch<2, T>>
         },
     Branch4 {
+        infix: [u8; 5],
         start_depth: u8,
         branch_depth: u8,
-        infix: [u8; 5],
-        ptr: NonNull<Branch<4>>,
-        phantom: PhantomData<Branch<4>>
+        ptr: NonNull<Branch<4, T>>,
+        phantom: PhantomData<Branch<4, T>>
         },
     Branch8 {
+        infix: [u8; 5],
         start_depth: u8,
         branch_depth: u8,
-        infix: [u8; 5],
-        ptr: NonNull<Branch<8>>,
-        phantom: PhantomData<Branch<8>>
+        ptr: NonNull<Branch<8, T>>,
+        phantom: PhantomData<Branch<8, T>>
         },
     Branch16 {
+        infix: [u8; 5],
         start_depth: u8,
         branch_depth: u8,
-        infix: [u8; 5],
-        ptr: NonNull<Branch<16>>,
-        phantom: PhantomData<Branch<16>>
+        ptr: NonNull<Branch<16, T>>,
+        phantom: PhantomData<Branch<16, T>>
         },
     Branch32 {
+        infix: [u8; 5],
         start_depth: u8,
         branch_depth: u8,
-        infix: [u8; 5],
-        ptr: NonNull<Branch<32>>,
-        phantom: PhantomData<Branch<32>>
+        ptr: NonNull<Branch<32, T>>,
+        phantom: PhantomData<Branch<32, T>>
         },
     Branch64 {
+        infix: [u8; 5],
         start_depth: u8,
         branch_depth: u8,
-        infix: [u8; 5],
-        ptr: NonNull<Branch<64>>,
-        phantom: PhantomData<Branch<64>>
+        ptr: NonNull<Branch<64, T>>,
+        phantom: PhantomData<Branch<64, T>>
         },
         /*
-    Infix2: {},
-    Infix3: {},
-    Infix4: {},
-    Leaf: {},
+    Infix14 {},
+    Infix30 {},
+    Infix46 {},
+    Infix62 {},
+
     */
+    Leaf {
+        infix: [u8; 8],
+        start_depth: u8,
+        value: T
+    },
 }
 
 
-unsafe impl ByteEntry for Head {
+unsafe impl<T: Clone> ByteEntry for Head<T> {
     fn zeroed() -> Self {
         return Head::None {padding: unsafe {mem::zeroed()}};
     }
@@ -97,7 +110,7 @@ unsafe impl ByteEntry for Head {
             Head::Branch16 { infix: infix, ..} => Some(infix[0]),
             Head::Branch32 { infix: infix, ..} => Some(infix[0]),
             Head::Branch64 { infix: infix, ..} => Some(infix[0]),
-
+            _ => None
 
         }
     }
@@ -110,18 +123,27 @@ mod tests {
 
     #[test]
     fn head_size() {
-        assert_eq!(mem::size_of::<Head>(), 16);
+        assert_eq!(mem::size_of::<Head<()>>(), 16);
     }
 
     #[test]
     fn branch_size() {
-        assert_eq!(mem::size_of::<ByteTable<1, Head>>(), 64);
-        assert_eq!(mem::size_of::<Branch<1>>(), 64*2);
-        assert_eq!(mem::size_of::<Branch<2>>(), 64*3);
-        assert_eq!(mem::size_of::<Branch<4>>(), 64*5);
-        assert_eq!(mem::size_of::<Branch<8>>(), 64*9);
-        assert_eq!(mem::size_of::<Branch<16>>(), 64*17);
-        assert_eq!(mem::size_of::<Branch<32>>(), 64*33);
-        assert_eq!(mem::size_of::<Branch<64>>(), 64*65);
+        assert_eq!(mem::size_of::<ByteTable<1, Head<()>>>(), 64);
+        assert_eq!(mem::size_of::<Branch<1, ()>>(), 64*2);
+        assert_eq!(mem::size_of::<Branch<2, ()>>(), 64*3);
+        assert_eq!(mem::size_of::<Branch<4, ()>>(), 64*5);
+        assert_eq!(mem::size_of::<Branch<8, ()>>(), 64*9);
+        assert_eq!(mem::size_of::<Branch<16, ()>>(), 64*17);
+        assert_eq!(mem::size_of::<Branch<32, ()>>(), 64*33);
+        assert_eq!(mem::size_of::<Branch<64, ()>>(), 64*65);
+        assert_eq!(mem::size_of::<Branch<64, ()>>(), 64*65);
+    }
+
+    #[test]
+    fn infix_size() {
+        assert_eq!(mem::size_of::<Infix<14, ()>>(), 16*2);
+        assert_eq!(mem::size_of::<Infix<30, ()>>(), 16*3);
+        assert_eq!(mem::size_of::<Infix<46, ()>>(), 16*4);
+        assert_eq!(mem::size_of::<Infix<62, ()>>(), 16*5);
     }
 }

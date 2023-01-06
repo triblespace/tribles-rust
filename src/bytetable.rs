f6df5da76ccb36b0175768b08e218685957c4be0
⚫️ <jp@bultmann.eu>
2023-01-06 20:25:41 +0100

    move bytetable to macros

--- src/bytetable.rs
+++ src/bytetable.rs
@@ -3,29 +3,11 @@ use rand::thread_rng;
 use std::mem;
 use std::sync::Once;
 
-//TODO: Try out an implementation where out of date
-// entries are deleted on growth.
-// This would allow us to get rid of the second (both?) bitmap(s).
-// And we could always delete the non ideal hashed version.
-
-/// The Trie's branching factor, fixed to the number of elements
-/// that can be represented by a byte/8bit.
-const BRANCH_FACTOR: usize = 256;
-
-/// The number of hashes used in the cuckoo table.
-const HASH_COUNT: usize = 2;
-
-/// The size of a cache line in bytes.
-const CACHE_LINE_SIZE: usize = 64;
-
-/// The size of bucket entries.
-const ENTRY_SIZE: usize = 16;
-
 /// The number of slots per bucket.
-const BUCKET_ENTRY_COUNT: usize = CACHE_LINE_SIZE / ENTRY_SIZE;
+const BUCKET_ENTRY_COUNT: usize = 4;
 
 /// The maximum number of buckets per table.
-const MAX_BUCKET_COUNT: usize = BRANCH_FACTOR / BUCKET_ENTRY_COUNT;
+const MAX_BUCKET_COUNT: usize = 256 / BUCKET_ENTRY_COUNT;
 
 /// The maximum number of cuckoo displacements attempted during
 /// insert before the size of the table is increased.
@@ -78,7 +60,7 @@ pub struct ByteBucket<T: ByteEntry + Clone> {
     entries: [T; BUCKET_ENTRY_COUNT],
 }
 
-impl<T: ByteEntry + Clone + std::fmt::Debug> ByteBucket<T> {
+impl<T: ByteEntry + Clone> ByteBucket<T> {
     fn new() -> Self {
         ByteBucket {
             entries: unsafe { mem::zeroed() },
@@ -136,114 +118,135 @@ fn compress_hash(bucket_count: usize, hash: usize) -> usize {
     hash & mask
 }
 
-#[derive(Clone, Debug)]
-#[repr(transparent)]
-pub struct ByteTable<const N: usize, T: ByteEntry + Clone> {
-    buckets: [ByteBucket<T>; N],
-}
-
-impl<const N: usize, T: ByteEntry + Clone + std::fmt::Debug> ByteTable<N, T> {
-    pub fn new() -> Self {
-        ByteTable {
-            buckets: unsafe { mem::zeroed() },
+macro_rules! create_bytetable {
+    // This macro takes an argument of designator `ident` and
+    // creates a function named `$func_name`.
+    // The `ident` designator is used for variable/function names.
+    ($name:ident, $size:expr) => {
+        #[derive(Clone, Debug)]
+        #[repr(transparent)]
+        pub struct $name<T: ByteEntry + Clone> {
+            buckets: [ByteBucket<T>; $size],
         }
-    }
 
-    pub fn get(&mut self, byte_key: u8) -> Option<&mut T> {
-        let ideal_entry = self.buckets[compress_hash(N, ideal_hash(byte_key))].get_key(byte_key);
-        if ideal_entry.is_some() {
-            return self.buckets[compress_hash(N, ideal_hash(byte_key))].get_key(byte_key);
-        }
-        let rand_entry = self.buckets[compress_hash(N, rand_hash(byte_key))].get_key(byte_key);
-        if rand_entry.is_some() {
-            return self.buckets[compress_hash(N, rand_hash(byte_key))].get_key(byte_key);
-        }
-        return None;
-    }
-
-    pub fn put(&mut self, entry: T) -> T {
-        if let Some(mut byte_key) = entry.key() {
-            if let Some(existing_entry) =
-                self.buckets[compress_hash(N, ideal_hash(byte_key))].get_key(byte_key)
-            {
-                mem::replace(existing_entry, entry);
-                return T::zeroed();
-            }
-            if let Some(existing_entry) =
-                self.buckets[compress_hash(N, rand_hash(byte_key))].get_key(byte_key)
-            {
-                let _ = mem::replace(existing_entry, entry);
-                return T::zeroed();
-            }
-
-            let max_grown = N != MAX_BUCKET_COUNT;
-            let min_grown = N == 1;
-
-            let mut use_ideal_hash = true;
-            let mut current_entry = entry;
-            let mut retries: usize = 0;
-            loop {
-                unsafe {
-                    RAND = RANDOM_PERMUTATION_RAND[(RAND ^ byte_key) as usize];
-                }
-
-                let hash = if use_ideal_hash {
-                    ideal_hash(byte_key)
-                } else {
-                    rand_hash(byte_key)
-                };
-                let bucket_index = compress_hash(N, hash);
-
-                if let Some(empty_entry) = self.buckets[bucket_index].get_empty() {
-                    return mem::replace(empty_entry, current_entry);
-                }
-
-                if min_grown || retries == MAX_RETRIES {
-                    return current_entry;
-                }
-
-                if max_grown {
-                    current_entry = self.buckets[bucket_index].shove_preserving_ideals(
-                        N,
-                        bucket_index,
-                        current_entry,
-                    );
-                    byte_key = current_entry.key().unwrap();
-                } else {
-                    retries += 1;
-                    current_entry = self.buckets[bucket_index].shove_randomly(current_entry);
-                    byte_key = current_entry.key().unwrap();
-                    use_ideal_hash = bucket_index != compress_hash(N, ideal_hash(byte_key));
+        impl<T: ByteEntry + Clone> $name<T> {
+            pub fn new() -> Self {
+                Self {
+                    buckets: unsafe { mem::zeroed() },
                 }
             }
-        } else {
-            return T::zeroed();
-        }
-    }
-    /*
-        // Contract: Key looked up must exist. Ensure with has.
-        unsafe fn get_existing(&self, byte_key: u8) -> Self::Entry;
 
-        // Contract: Key looked up must exist. Ensure with has.
-        unsafe fn put_existing(&mut self, entry: T);
-    */
-    pub unsafe fn grow_repair(&mut self) {
-        assert!(N % 2 == 0);
-        let (old_portion, new_portion) = self.buckets.split_at_mut(N / 2);
-        for bucket_index in 0..N / 2 {
-            for entry in &mut old_portion[bucket_index].entries {
-                if let Some(byte_key) = entry.key() {
-                    let ideal_index = compress_hash(N, ideal_hash(byte_key));
-                    let rand_index = compress_hash(N, rand_hash(byte_key));
-                    if bucket_index == ideal_index || bucket_index == rand_index {
-                        continue;
+            pub fn get(&mut self, byte_key: u8) -> Option<&mut T> {
+                let ideal_entry =
+                    self.buckets[compress_hash($size, ideal_hash(byte_key))].get_key(byte_key);
+                if ideal_entry.is_some() {
+                    return self.buckets[compress_hash($size, ideal_hash(byte_key))]
+                        .get_key(byte_key);
+                }
+                let rand_entry =
+                    self.buckets[compress_hash($size, rand_hash(byte_key))].get_key(byte_key);
+                if rand_entry.is_some() {
+                    return self.buckets[compress_hash($size, rand_hash(byte_key))]
+                        .get_key(byte_key);
+                }
+                return None;
+            }
+
+            pub fn put(&mut self, entry: T) -> T {
+                if let Some(mut byte_key) = entry.key() {
+                    if let Some(existing_entry) =
+                        self.buckets[compress_hash($size, ideal_hash(byte_key))].get_key(byte_key)
+                    {
+                        mem::replace(existing_entry, entry);
+                        return T::zeroed();
                     }
-                    mem::swap(entry, new_portion[bucket_index].get_empty().unwrap());
+                    if let Some(existing_entry) =
+                        self.buckets[compress_hash($size, rand_hash(byte_key))].get_key(byte_key)
+                    {
+                        let _ = mem::replace(existing_entry, entry);
+                        return T::zeroed();
+                    }
+
+                    let max_grown = $size == MAX_BUCKET_COUNT;
+                    let min_grown = $size == 1;
+
+                    let mut use_ideal_hash = true;
+                    let mut current_entry = entry;
+                    let mut retries: usize = 0;
+                    loop {
+                        unsafe {
+                            RAND = RANDOM_PERMUTATION_RAND[(RAND ^ byte_key) as usize];
+                        }
+
+                        let hash = if use_ideal_hash {
+                            ideal_hash(byte_key)
+                        } else {
+                            rand_hash(byte_key)
+                        };
+                        let bucket_index = compress_hash($size, hash);
+
+                        if let Some(empty_entry) = self.buckets[bucket_index].get_empty() {
+                            return mem::replace(empty_entry, current_entry);
+                        }
+
+                        if min_grown || retries == MAX_RETRIES {
+                            return current_entry;
+                        }
+
+                        if max_grown {
+                            current_entry = self.buckets[bucket_index].shove_preserving_ideals(
+                                $size,
+                                bucket_index,
+                                current_entry,
+                            );
+                            byte_key = current_entry.key().unwrap();
+                        } else {
+                            retries += 1;
+                            current_entry =
+                                self.buckets[bucket_index].shove_randomly(current_entry);
+                            byte_key = current_entry.key().unwrap();
+                            use_ideal_hash =
+                                bucket_index != compress_hash($size, ideal_hash(byte_key));
+                        }
+                    }
+                } else {
+                    return T::zeroed();
+                }
+            }
+            /*
+                // Contract: Key looked up must exist. Ensure with has.
+                unsafe fn get_existing(&self, byte_key: u8) -> Self::Entry;
+
+                // Contract: Key looked up must exist. Ensure with has.
+                unsafe fn put_existing(&mut self, entry: T);
+            */
+            pub unsafe fn grow_repair(&mut self) {
+                assert!($size % 2 == 0);
+                let (old_portion, new_portion) = self.buckets.split_at_mut($size / 2);
+                for bucket_index in 0..$size / 2 {
+                    for entry in &mut old_portion[bucket_index].entries {
+                        if let Some(byte_key) = entry.key() {
+                            let ideal_index = compress_hash($size, ideal_hash(byte_key));
+                            let rand_index = compress_hash($size, rand_hash(byte_key));
+                            if bucket_index == ideal_index || bucket_index == rand_index {
+                                continue;
+                            }
+                            mem::swap(entry, new_portion[bucket_index].get_empty().unwrap());
+                        }
+                    }
                 }
             }
         }
-    }
+    };
 }
+
+create_bytetable!(ByteTable4, 1);
+create_bytetable!(ByteTable8, 2);
+create_bytetable!(ByteTable16, 4);
+create_bytetable!(ByteTable32, 8);
+create_bytetable!(ByteTable64, 16);
+create_bytetable!(ByteTable128, 32);
+create_bytetable!(ByteTable256, 64);
 
 #[cfg(test)]
 mod tests {
@@ -295,21 +298,21 @@ mod tests {
     #[test]
     fn new_empty_table() {
         init();
-        let table: ByteTable<1, DummyEntry> = ByteTable::new();
+        let table: ByteTable4<DummyEntry> = ByteTable4::new();
     }
 
     proptest! {
         #[test]
         fn empty_table_then_empty_get(n in 0u8..255) {
             init();
-            let mut table: ByteTable<1, DummyEntry> = ByteTable::new();
+            let mut table: ByteTable4<DummyEntry> = ByteTable4::new();
             prop_assert!(table.get(n).is_none());
         }
 
         #[test]
         fn single_put_success(n in 0u8..255) {
             init();
-            let mut table: ByteTable<1, DummyEntry> = ByteTable::new();
+            let mut table: ByteTable4<DummyEntry> = ByteTable4::new();
             let entry = DummyEntry::new(n);
             let displaced = table.put(entry);
             prop_assert!(displaced.key().is_none());

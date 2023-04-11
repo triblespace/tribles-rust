642940df7feaa159082c41daf25db27a19a008d1
⚫️ <jp@bultmann.eu>
2023-04-11 12:53:24 +0200

    namespace wip

--- src/namespace.rs
+++ src/namespace.rs
@@ -42,7 +42,7 @@ pub fn factory() -> Value {
 
 mod knightsNS {
     pub mod id {
-        pub use crate::namespace::factory;
+        pub use crate::namespace::factory as factory;
     }
     pub mod ids {
         use hex_literal::hex;
@@ -50,53 +50,75 @@ mod knightsNS {
         pub const loves: [u8; 16] = hex!("328edd7583de04e2bedd6bd4fd50e651");
         pub const title: [u8; 16] = hex!("328f2c33d2fdd675e733388770b2d6c4");
     }
-    pub mod encoders {
+    pub mod types {
         pub use crate::namespace::encode_id as loves;
         pub use crate::namespace::encode_string as name;
         pub use crate::namespace::encode_string as title;
     }
-
-    macro_rules! entity {
-        ({@:$EntityId:expr, $($FieldName:ident : $Value:expr),*}) => {
-            {
-                [$(($EntityId,
-                    crate::namespace::knightsNS::ids::$FieldName,
-                    crate::namespace::knightsNS::encoders::$FieldName($Value.into()))),*]
-            }
-        };
-        ({$($FieldName:ident : $Value:expr),*}) => {
-            {
-                {let id = "newid";
-                    [$((id,
-                        crate::namespace::knightsNS::ids::$FieldName,
-                        crate::namespace::knightsNS::encoders::$FieldName($Value.into()))),*]
-                }
-            }
-        };
-    }
-    pub(crate) use entity;
-
-    macro_rules! entities {
-        (($($Var:ident),*), [$($Entity:tt),*]) => {
-            {
-                $(let $Var = crate::namespace::knightsNS::id::factory();)*
-                [$(entity!($Entity)),*]
-            }
-        };
-    }
-    pub(crate) use entities;
 }
+/*
+NS!{
+    knights {
+        @: UFOID,
+        name: String @ "328147856cc1984f0806dbb824d2b4cb",
+        loves @ "328edd7583de04e2bedd6bd4fd50e651",
+        lovedBy @ inv "328edd7583de04e2bedd6bd4fd50e651",
+    }
+}
+ 
+macro_rules! NS {
+    ({$($FieldName:ident : $Value:expr),*}) => {
+        {
+            [$(($EntityId,
+                { use $Namespace as base; base::ids::$FieldName },
+                { use $Namespace as base; base::encoders::$FieldName($Value.into()) })),*]
+        }
+    };
+}
+pub(crate) use NS;
+*/
+
+macro_rules! entity {
+    ($Namespace:path, {@:$EntityId:expr, $($FieldName:ident : $Value:expr),*}) => {
+        {
+            [$(($EntityId,
+                { use $Namespace as base; base::ids::$FieldName },
+                { use $Namespace as base; base::encoders::$FieldName($Value.into()) })),*]
+        }
+    };
+    ($Namespace:path, {$($FieldName:ident : $Value:expr),*}) => {
+        {
+            {let id = { use $Namespace as base; base::id::factory() };
+                [$((id,
+                    { use $Namespace as base; base::ids::$FieldName },
+                    { use $Namespace as base; base::encoders::$FieldName($Value.into()) })),*]
+            }
+        }
+    };
+}
+pub(crate) use entity;
+
+macro_rules! entities {
+    ($Namespace:path, ($($Var:ident),*), [$($Entity:tt),*]) => {
+        {
+            $(let $Var = { use $Namespace as base; base::id::factory() };)*
+            [$(entity!($Namespace, $Entity)),*]
+        }
+    };
+}
+pub(crate) use entities;
 
 #[cfg(test)]
 mod tests {
-    use super::knightsNS::entities;
-    use super::knightsNS::entity;
+    use super::entities;
+    use super::entity;
+    use super::knightsNS;
 
     #[test]
     fn ns_entity() {
         println!(
             "{:?}",
-            entity!({
+            entity!(knightsNS, {
                 @:"32d86c15fa6818b8335d15ff39281ec1",
                 name: "Romeo",
                 loves: [0;32],
@@ -109,7 +131,7 @@ mod tests {
     fn ns_entities() {
         println!(
             "{:?}",
-            entities!((romeo, juliet),
+            entities!(knightsNS, (romeo, juliet),
                 [{
                 @:romeo,
                 name: "Romeo",

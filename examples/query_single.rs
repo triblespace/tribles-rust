2669c5c4434aa3172e51ac56ef0ca2f6098deb47
⚫️ <jp@bultmann.eu>
2023-11-02 23:54:23 +0100

    add single element query

---
+++ examples/query_single.rs
@@ -0,0 +1,55 @@
+use std::convert::TryInto;
+
+use tribles::namespace::knights;
+use tribles::query;
+
+use tribles::patch;
+use tribles::tribleset::patchtribleset::PATCHTribleSet;
+
+use fake::faker::name::raw::*;
+use fake::locales::*;
+use fake::Fake;
+
+fn main() {
+    patch::init();
+
+    let mut kb = PATCHTribleSet::new();
+    (0..1000000).for_each(|_| {
+        kb.union(&knights::entities!((lover_a, lover_b),
+        [{lover_a @
+            name: Name(EN).fake::<String>().try_into().unwrap(),
+            loves: lover_b
+        },
+        {lover_b @
+            name: Name(EN).fake::<String>().try_into().unwrap(),
+            loves: lover_a
+        }]));
+    });
+
+    let data_kb = knights::entities!((romeo, juliet),
+    [{juliet @
+        name: "Juliet".try_into().unwrap(),
+        loves: romeo
+    },
+    {romeo @
+        name: "Romeo".try_into().unwrap(),
+        loves: juliet
+    }]);
+
+    kb.union(&data_kb);
+
+    loop {
+        for _r in query!(
+            ctx,
+            (juliet, name),
+            knights::pattern!(ctx, kb, [
+            {name: ("Romeo".try_into().unwrap()),
+             loves: juliet},
+            {juliet @
+                name: name
+            }])
+        ) {
+            coz::progress!();
+        }
+    }
+}

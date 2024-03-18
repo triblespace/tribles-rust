338b455a57379b0607d15e839eb20dd1badd22e2
⚫️ <jp@bultmann.eu>
2024-03-18 14:41:51 +0100

    add time interval

--- src/types/time.rs
+++ src/types/time.rs
@@ -1,29 +1,60 @@
-use crate::Valuelike;
+use std::convert::TryInto;
 
-use uom::si::i128::Time;
-use uom::si::time::nanosecond;
+use crate::{ValueParseError, Valuelike};
 
-/*
-impl<Tz> Valuelike for DateTime<Tz>
-where Tz: TimeZone {
+use hifitime::prelude::*;
+
+pub struct NsTAIInterval(pub i128, pub i128);
+
+impl Valuelike for NsTAIInterval {
     fn from_value(value: crate::Value) -> Result<Self, crate::ValueParseError> {
-        todo!()
+        let lower = i128::from_be_bytes(value[0..16].try_into().unwrap());
+        let upper = i128::from_be_bytes(value[16..32].try_into().unwrap());
+        Ok(NsTAIInterval(lower, upper))
     }
 
     fn into_value(v: &Self) -> crate::Value {
-        todo!()
+        let mut value = [0; 32];
+        value[0..16].copy_from_slice(&v.0.to_be_bytes());
+        value[16..32].copy_from_slice(&v.1.to_be_bytes());
+        value
     }
 }
-*/
+
+impl From<(Epoch, Epoch)> for NsTAIInterval {
+    fn from(value: (Epoch, Epoch)) -> Self {
+        let lower = value.0.to_tai_duration().total_nanoseconds();
+        let upper = value.1.to_tai_duration().total_nanoseconds();
+
+        NsTAIInterval(lower, upper)
+    }
+}
+
+impl From<NsTAIInterval> for (Epoch, Epoch) {
+    fn from(value: NsTAIInterval) -> Self {
+        let lower = Epoch::from_tai_duration(Duration::from_total_nanoseconds(value.0));
+        let upper = Epoch::from_tai_duration(Duration::from_total_nanoseconds(value.1));
+
+        (lower, upper)
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn nanosecond_interval() {
-        let time = Time::new::<nanosecond>(1);
-        let v = time.value;
-        println!("{} nanos", v);
+    fn tai_nanosecond_interval() {
+        let epoch: NsTAIInterval = NsTAIInterval(0, 0);
+        let value: [u8; 32] = NsTAIInterval::into_value(&epoch);
+        let _ = NsTAIInterval::from_value(value);
+    }
+
+    #[test]
+    fn hifitime_conversion() {
+        let epoch: NsTAIInterval = NsTAIInterval(0, 0);
+        let time: (Epoch, Epoch) = epoch.into();
+        let _: NsTAIInterval = time.into();
+        
     }
 }

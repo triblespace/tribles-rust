df30d2886daac9131b1f5100ddccde2c550ddd73
⚫️ <jp@bultmann.eu>
2024-05-08 22:02:23 +0200

    small improvements

--- src/tribleset/triblesetarchive.rs
+++ src/tribleset/triblesetarchive.rs
@@ -30,7 +30,7 @@ use super::TribleSet;
 pub trait Universe {
     fn with<I>(iter: I) -> Self
     where I: Iterator<Item = Value>;
-    fn access(&self, pos: usize) -> Option<Value>;
+    fn access(&self, pos: usize) -> Value;
     fn search(&self, v: &Value) -> Option<usize>;
     fn size_in_bytes(&self) -> usize;
     fn len(&self) -> usize;
@@ -49,8 +49,8 @@ impl Universe for OrderedUniverse {
         }
     }
 
-    fn access(&self, pos: usize) -> Option<Value> {
-        self.values.get(pos).copied()
+    fn access(&self, pos: usize) -> Value {
+        self.values[pos]
     }
 
     fn search(&self, v: &Value) -> Option<usize> {
@@ -68,7 +68,7 @@ impl Universe for OrderedUniverse {
 
 #[derive(Debug, Clone)]
 pub struct CompressedUniverse<C> {
-    columns: Vec<C>,
+    segments: Vec<C>,
 }
 
 impl<C> Universe for CompressedUniverse<C>
@@ -91,39 +91,39 @@ where C: IBuild + IAccess + NumVals + Serializable {
         }
 
         Self {
-            columns
+            segments: columns
         }
     }
 
-    fn access(&self, pos: usize) -> Option<Value> {
+    fn access(&self, pos: usize) -> Value {
         let mut v: Value = [0; 32];
 
-        v[0..8].copy_from_slice(&(self.columns[0].access(pos)?.to_be_bytes()));
-        v[8..16].copy_from_slice(&(self.columns[1].access(pos)?.to_be_bytes()));
-        v[16..24].copy_from_slice(&(self.columns[2].access(pos)?.to_be_bytes()));
-        v[24..32].copy_from_slice(&(self.columns[3].access(pos)?.to_be_bytes()));
+        v[0..8].copy_from_slice(&(self.segments[0].access(pos).unwrap().to_be_bytes()));
+        v[8..16].copy_from_slice(&(self.segments[1].access(pos).unwrap().to_be_bytes()));
+        v[16..24].copy_from_slice(&(self.segments[2].access(pos).unwrap().to_be_bytes()));
+        v[24..32].copy_from_slice(&(self.segments[3].access(pos).unwrap().to_be_bytes()));
 
-        Some(v)
+        v
     }
 
     fn search(&self, v: &Value) -> Option<usize> {
-        (0..=self.columns[0].num_vals()-1).binary_by(|p| self.access(p).unwrap().cmp(v)).ok()
+        (0..=self.segments[0].num_vals()-1).binary_by(|p| self.access(p).cmp(v)).ok()
         /* //TODO fix this and bench.
         let v0 = usize::from_be_bytes(v[0..8].try_into().unwrap());
         let v1 = usize::from_be_bytes(v[8..16].try_into().unwrap());
         let v2 = usize::from_be_bytes(v[16..24].try_into().unwrap());
         let v3 = usize::from_be_bytes(v[24..32].try_into().unwrap());
 
-        let r0 = (0..=self.columns[0].num_vals()-1).binary_all(|p| self.columns[0].access(p).unwrap().cmp(&v0));
+        let r0 = (0..=self.segments[0].num_vals()-1).binary_all(|p| self.segments[0].access(p).unwrap().cmp(&v0));
         if r0.is_empty() { return None;}
 
-        let r1 = (r0.start..=r0.end-1).binary_all(|p| self.columns[1].access(p).unwrap().cmp(&v1));
+        let r1 = (r0.start..=r0.end-1).binary_all(|p| self.segments[1].access(p).unwrap().cmp(&v1));
         if r1.is_empty() { return None;}
 
-        let r2 = (r1.start..=r1.end-1).binary_all(|p| self.columns[2].access(p).unwrap().cmp(&v2));
+        let r2 = (r1.start..=r1.end-1).binary_all(|p| self.segments[2].access(p).unwrap().cmp(&v2));
         if r2.is_empty() { return None;}
 
-        let r3 = (r2.start..=r2.end-1).binary_all(|p| self.columns[3].access(p).unwrap().cmp(&v3));
+        let r3 = (r2.start..=r2.end-1).binary_all(|p| self.segments[3].access(p).unwrap().cmp(&v3));
         if r3.is_empty() { return None;}
 
         assert!(r3.len() == 1);
@@ -133,12 +133,12 @@ where C: IBuild + IAccess + NumVals + Serializable {
     }
 
     fn size_in_bytes(&self) -> usize {
-        self.columns.iter().map(|c| c.size_in_bytes()).sum()
+        self.segments.iter().map(|c| c.size_in_bytes()).sum()
 
     }
 
     fn len(&self) -> usize {
-        self.columns[0].num_vals()
+        self.segments[0].num_vals()
     }
 }
 
@@ -330,7 +330,7 @@ mod tests {
             values.sort();
             let u = OrderedUniverse::with(values.iter().copied());
             for i in 0..u.len() {
-                let original = Some(values[i]);
+                let original = values[i];
                 let reconstructed = u.access(i);
                 assert_eq!(original, reconstructed);
             }
@@ -347,7 +347,7 @@ mod tests {
             values.sort();
             let u = CompressedUniverse::<DacsOpt>::with(values.iter().copied());
             for i in 0..u.len() {
-                let original = Some(values[i]);
+                let original = values[i];
                 let reconstructed = u.access(i);
                 assert_eq!(original, reconstructed);
             }

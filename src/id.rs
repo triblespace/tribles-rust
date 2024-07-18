7e7509492cb86ec81ced809a9a9e4ff0cd3c1b12
⚫️ <jp@bultmann.eu>
2024-07-18 15:29:17 +0200

    make RawId RawValue converison private

--- src/id.rs
+++ src/id.rs
@@ -17,13 +17,13 @@ use crate::VALUE_LEN;
 pub const ID_LEN: usize = 16;
 pub type RawId = [u8; ID_LEN];
 
-pub fn id_into_value(id: RawId) -> RawValue {
+pub(crate) fn id_into_value(id: RawId) -> RawValue {
     let mut data = [0; VALUE_LEN];
     data[16..32].copy_from_slice(&id[..]);
     data
 }
 
-pub fn id_from_value(id: RawValue) -> Option<RawId> {
+pub(crate) fn id_from_value(id: RawValue) -> Option<RawId> {
     if id[0..16] != [0; 16] {
         return None;
     }

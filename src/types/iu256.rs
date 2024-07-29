bc761c6e986c4d17adf6aaa70bc702b15dcb58f6
⚫️ <jp@bultmann.eu>
2024-07-29 20:09:24 +0200

    add conversions for numbers to UI256

--- src/types/iu256.rs
+++ src/types/iu256.rs
@@ -57,3 +57,123 @@ impl From<Value<I256LE>> for ethnum::I256 {
         ethnum::I256::from_le_bytes(value.bytes)
     }
 }
+
+impl From<u8> for Value<U256LE> {
+    fn from(value: u8) -> Self {
+        Value::new(ethnum::U256::new(value.into()).to_le_bytes())
+    }
+}
+
+impl From<u16> for Value<U256LE> {
+    fn from(value: u16) -> Self {
+        Value::new(ethnum::U256::new(value.into()).to_le_bytes())
+    }
+}
+
+impl From<u32> for Value<U256LE> {
+    fn from(value: u32) -> Self {
+        Value::new(ethnum::U256::new(value.into()).to_le_bytes())
+    }
+}
+
+impl From<u64> for Value<U256LE> {
+    fn from(value: u64) -> Self {
+        Value::new(ethnum::U256::new(value.into()).to_le_bytes())
+    }
+}
+
+impl From<u128> for Value<U256LE> {
+    fn from(value: u128) -> Self {
+        Value::new(ethnum::U256::new(value.into()).to_le_bytes())
+    }
+}
+
+impl From<u8> for Value<U256BE> {
+    fn from(value: u8) -> Self {
+        Value::new(ethnum::U256::new(value.into()).to_be_bytes())
+    }
+}
+
+impl From<u16> for Value<U256BE> {
+    fn from(value: u16) -> Self {
+        Value::new(ethnum::U256::new(value.into()).to_be_bytes())
+    }
+}
+
+impl From<u32> for Value<U256BE> {
+    fn from(value: u32) -> Self {
+        Value::new(ethnum::U256::new(value.into()).to_be_bytes())
+    }
+}
+
+impl From<u64> for Value<U256BE> {
+    fn from(value: u64) -> Self {
+        Value::new(ethnum::U256::new(value.into()).to_be_bytes())
+    }
+}
+
+impl From<u128> for Value<U256BE> {
+    fn from(value: u128) -> Self {
+        Value::new(ethnum::U256::new(value.into()).to_be_bytes())
+    }
+}
+
+impl From<i8> for Value<I256LE> {
+    fn from(value: i8) -> Self {
+        Value::new(ethnum::I256::new(value.into()).to_le_bytes())
+    }
+}
+
+impl From<i16> for Value<I256LE> {
+    fn from(value: i16) -> Self {
+        Value::new(ethnum::I256::new(value.into()).to_le_bytes())
+    }
+}
+
+impl From<i32> for Value<I256LE> {
+    fn from(value: i32) -> Self {
+        Value::new(ethnum::I256::new(value.into()).to_le_bytes())
+    }
+}
+
+impl From<i64> for Value<I256LE> {
+    fn from(value: i64) -> Self {
+        Value::new(ethnum::I256::new(value.into()).to_le_bytes())
+    }
+}
+
+impl From<i128> for Value<I256LE> {
+    fn from(value: i128) -> Self {
+        Value::new(ethnum::I256::new(value.into()).to_le_bytes())
+    }
+}
+
+impl From<i8> for Value<I256BE> {
+    fn from(value: i8) -> Self {
+        Value::new(ethnum::I256::new(value.into()).to_be_bytes())
+    }
+}
+
+impl From<i16> for Value<I256BE> {
+    fn from(value: i16) -> Self {
+        Value::new(ethnum::I256::new(value.into()).to_be_bytes())
+    }
+}
+
+impl From<i32> for Value<I256BE> {
+    fn from(value: i32) -> Self {
+        Value::new(ethnum::I256::new(value.into()).to_be_bytes())
+    }
+}
+
+impl From<i64> for Value<I256BE> {
+    fn from(value: i64) -> Self {
+        Value::new(ethnum::I256::new(value.into()).to_be_bytes())
+    }
+}
+
+impl From<i128> for Value<I256BE> {
+    fn from(value: i128) -> Self {
+        Value::new(ethnum::I256::new(value.into()).to_be_bytes())
+    }
+}

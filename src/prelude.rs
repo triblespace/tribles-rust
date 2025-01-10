5bd8c87f1e403cb7d03e49d73871e7453577c75b
⚫️ <jp@bultmann.eu>
2025-01-10 18:20:20 +0100

    improve docs

--- src/prelude.rs
+++ src/prelude.rs
@@ -8,28 +8,24 @@
 //!
 //! # Deletion and Forgetting
 //!
-//! On the surface deletion and forgetting seem to be the same thing.
-//! However there is a subtle difference: When you delete something,
-//! you say that it no longer exists. When you forget something, you
-//! say that you no longer know about it. This subtle difference is
-//! important when you are working with data that is shared between
-//! multiple parties. When you delete something, you are saying that
-//! it no longer exists for anyone, and deleting a statement or fact
-//! implies that it is no longer true. When you forget something, you
-//! are saying that you no longer know about it, but the statement or
-//! fact is still generally valid. More importantly, when you forget
-//! something, you are not saying that others should forget it as well,
-//! nor does it imply that statements or facts derived from the forgotten
-//! statement or fact are no longer valid.
-//!
-//! This property is called _monotonicity_, and it has a deep relationship
-//! with _consistency_, as laid out by the [CALM theorem](https://arxiv.org/abs/1901.01930)
+//! On the surface, deletion and forgetting may seem identical.
+//! However, there is a subtle but crucial difference: Deletion removes
+//! a statement from existence, making it no longer valid, whereas forgetting removes your knowledge
+//! of it, without affecting it's validity. This distinction is particularly
+//! important in contexts where data is shared among multiple parties, or where
+//! derived statements are based on the original data.
+//! Forgetting does not propagate to other parties,
+//! is reversible should the forgotten information be rediscovered,
+//! and does not invalidate any derived statements or facts.
+//! 
+//! The property that distinguishes forgetting from deletion is called _monotonicity_,
+//! and it has a deep relationship with _consistency_, as laid out by the [CALM theorem](https://arxiv.org/abs/1901.01930)
 //! (Consistency as Logical Monotonicity). The CALM theorem states that a distributed
 //! system is consistent if and only if it is logically monotonic. This means that
 //! if you want to build a consistent distributed system, you need to
 //! ensure that it is logically monotonic. This is where forgetting comes
-//! in: By allowing you to forget things, but preventing you from deleting
-//! things, `tribles` allows you to build consistent distributed systems.
+//! in: _By allowing you to forget things, but preventing you from deleting
+//! things, `tribles` allows you to build consistent distributed systems._
 //!
 
 pub mod blobschemas;

d68b17179ce64e7c6be9606a51300a95eb9304d6
⚫️ <jp@bultmann.eu>
2025-04-25 12:31:05 +0200

    fix benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -743,7 +743,7 @@ fn pile_benchmark(c: &mut Criterion) {
             |data: Vec<Bytes>| {
                 let tmp_dir = tempfile::tempdir().unwrap();
                 let tmp_pile = tmp_dir.path().join("test.pile");
-                let pile: Pile<MAX_PILE_SIZE> = Pile::open(&tmp_pile).unwrap();
+                let mut pile: Pile<MAX_PILE_SIZE> = Pile::open(&tmp_pile).unwrap();
                 data.iter().for_each(|data| {
                     pile.insert_blob(UnknownBlob::blob_from(data.clone()))
                         .unwrap();
@@ -801,7 +801,7 @@ fn pile_benchmark(c: &mut Criterion) {
             |data: Vec<Bytes>| {
                 let tmp_dir = tempfile::tempdir().unwrap();
                 let tmp_pile = tmp_dir.path().join("test.pile");
-                let pile: Pile<MAX_PILE_SIZE> = Pile::open(&tmp_pile).unwrap();
+                let mut pile: Pile<MAX_PILE_SIZE> = Pile::open(&tmp_pile).unwrap();
                 data.iter().for_each(|data| {
                     pile.insert_blob(UnknownBlob::blob_from(data.clone()))
                         .unwrap();
@@ -819,7 +819,7 @@ fn pile_benchmark(c: &mut Criterion) {
                 let mut rng = rand::thread_rng();
                 let tmp_dir = tempfile::tempdir().unwrap();
                 let tmp_pile = tmp_dir.path().join("test.pile");
-                let pile: Pile<MAX_PILE_SIZE> = Pile::open(&tmp_pile).unwrap();
+                let mut pile: Pile<MAX_PILE_SIZE> = Pile::open(&tmp_pile).unwrap();
 
                 (0..RECORD_COUNT).for_each(|_| {
                     let mut record = vec![0u8; RECORD_LEN];

a44d1d55e15da2b4c33701e361e0369678ceb06c
⚫️ <jp@bultmann.eu>
2025-07-04 14:15:01 +0200

    add playback for failing kani proof
    my hunch is that we're checking strings that have multiple NULL-bytes in teir contents

--- proofs/value_harness.rs
+++ proofs/value_harness.rs
@@ -13,3 +13,78 @@ fn short_string_roundtrip() {
     assert!(result.is_ok());
     assert_eq!(result.unwrap(), s.as_str());
 }
+
+/// Test generated for harness `proofs::value_harness::short_string_roundtrip`
+///
+/// Check for `assertion`: "This is a placeholder message; Kani doesn't support message formatted at runtime"
+
+#[test]
+fn kani_concrete_playback_short_string_roundtrip_10333782728379354304() {
+    let concrete_vals: Vec<Vec<u8>> = vec![
+        // 0
+        vec![0],
+        // 128
+        vec![128],
+        // 192
+        vec![192],
+        // 0
+        vec![0],
+        // 192
+        vec![192],
+        // 192
+        vec![192],
+        // 127
+        vec![127],
+        // 193
+        vec![193],
+        // 192
+        vec![192],
+        // 127
+        vec![127],
+        // 192
+        vec![192],
+        // 192
+        vec![192],
+        // 127
+        vec![127],
+        // 192
+        vec![192],
+        // 192
+        vec![192],
+        // 127
+        vec![127],
+        // 192
+        vec![192],
+        // 192
+        vec![192],
+        // 127
+        vec![127],
+        // 192
+        vec![192],
+        // 192
+        vec![192],
+        // 127
+        vec![127],
+        // 255
+        vec![255],
+        // 192
+        vec![192],
+        // 0
+        vec![0],
+        // 192
+        vec![192],
+        // 192
+        vec![192],
+        // 0
+        vec![0],
+        // 192
+        vec![192],
+        // 192
+        vec![192],
+        // 0
+        vec![0],
+        // 192
+        vec![192],
+    ];
+    kani::concrete_playback_run(concrete_vals, short_string_roundtrip);
+}

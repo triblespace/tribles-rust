2da6a2e5263f977d226081631d9e3ec58d8360df
⚫️ <jp@bultmann.eu>
2025-07-26 01:41:50 +0200

    add experimental two stage sorting based on magnitude and variable influence

--- src/query.rs
+++ src/query.rs
@@ -467,7 +467,7 @@ impl<'a, C: Constraint<'a>, P: Fn(&Binding) -> R, R> Query<C, P, R> {
             }
         });
         let mut unbound = ArrayVec::from_iter(variables);
-        unbound.sort_unstable_by_key(|v| Reverse(estimates[*v]));
+        unbound.sort_unstable_by_key(|v| (Reverse(estimates[*v].ilog2()), influences[*v].count()));
 
         Query {
             constraint,
@@ -532,8 +532,7 @@ impl<'a, C: Constraint<'a>, P: Fn(&Binding) -> R, R> Iterator for Query<C, P, R>
                         }
 
                         self.unbound
-                            .sort_unstable_by_key(|v| (Reverse(self.estimates[*v]), self.influences[*v].count()));
-                        // Idea: We could also sort the unbound variables by their influence.
+                            .sort_unstable_by_key(|v| (Reverse(self.estimates[*v].ilog2()), self.influences[*v].count()));
                     }
 
                     let variable = self.unbound.pop().expect("non-empty unbound");

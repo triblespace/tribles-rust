0ce2bb3d0bf1c4e40b32fb84a868ca8485980c46
Jan-Paul Bultmann <74891396+somethingelseentirely@users.noreply.github.com>
2025-08-29 15:12:26 +0200

    Refresh branches on access

--- src/repo/hybridstore.rs
+++ src/repo/hybridstore.rs
@@ -75,11 +75,11 @@ where
         R: 'a,
         B: 'a;
 
-    fn branches<'a>(&'a self) -> Self::ListIter<'a> {
+    fn branches<'a>(&'a mut self) -> Result<Self::ListIter<'a>, Self::BranchesError> {
         self.branches.branches()
     }
 
-    fn head(&self, id: Id) -> Result<Option<Value<Handle<H, SimpleArchive>>>, Self::HeadError> {
+    fn head(&mut self, id: Id) -> Result<Option<Value<Handle<H, SimpleArchive>>>, Self::HeadError> {
         self.branches.head(id)
     }
 
--- src/repo/objectstore.rs
+++ src/repo/objectstore.rs
@@ -170,7 +170,7 @@ where
 
     type ListIter<'a> = BlockingIter<Result<Id, Self::BranchesError>>;
 
-    fn branches<'a>(&'a self) -> Self::ListIter<'a> {
+    fn branches<'a>(&'a mut self) -> Result<Self::ListIter<'a>, Self::BranchesError> {
         let prefix = self.prefix.child(BRANCH_INFIX);
         let stream = self.store.list(Some(&prefix)).map(|r| match r {
             Ok(meta) => {
@@ -186,10 +186,10 @@ where
             }
             Err(e) => Err(ListBranchesErr::List(e)),
         });
-        BlockingIter::new(stream)
+        Ok(BlockingIter::new(stream))
     }
 
-    fn head(&self, id: Id) -> Result<Option<Value<Handle<H, SimpleArchive>>>, Self::HeadError> {
+    fn head(&mut self, id: Id) -> Result<Option<Value<Handle<H, SimpleArchive>>>, Self::HeadError> {
         let path = self.prefix.child(BRANCH_INFIX).child(hex::encode(id));
         let result = block_on(async { self.store.get(&path).await });
         match result {
